eac85e5d0a26bdf6f9c2408fa9ed15702731ca24
daniela <dantasdaniela170994@gmail.com>
2024-04-11 14:31:40 -0300

    main

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <section id="app" className='container'>
+        <header>
+          <h1 className='title'>Lista de tarefas</h1>
+        </header>
+        <div className='main' >
+          
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
